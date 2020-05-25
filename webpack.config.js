f00eb0c5cc856642455ef525c01d0deb9bc1e9a6
Lux <luxliu007168@gmail.com>
2020-05-25 17:59:21 +1200

    finish

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,20 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
   output: {
